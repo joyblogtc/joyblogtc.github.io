43eed01c7b3afd09ea558876e0b838227b5e684a
joyblogtc <126516625+joyblogtc@users.noreply.github.com>
2023-03-12 20:51:09 +0000

    Site updated: 2023-03-12 20:51:07

---
+++ banner/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+  var infoBar = document.querySelector(".cookie-consent-banner");
+  var btnAccept = document.querySelector(".cookie-consent-banner__cta");
+  // var btnReject = document.querySelector(".cookie-consent-banner__cta--secondary");
+
+
+  // Check if user has already accepted the notification
+  if(wasAccepted()) {
+    hideInfobar();
+    return;
+  }
+
+  // btnReject.addEventListener("click", function (e) {
+  //    e.preventDefault();
+  //    hideInfobar();
+  //  });
+
+  //listen for the click event on Accept button
+  btnAccept.addEventListener("click", function (e) {
+    e.preventDefault();
+    hideInfobar();
+    saveAcceptInCookies();
+  });
+
+  //hide cookie info bar
+  function hideInfobar () {
+    infoBar.className = infoBar.classList.value + " cookies-consent-banner_accepted";
+  }
+
+  // Check if user has already accepted the notification
+   function wasAccepted () {
+     return checkCookie() === "1";
+   }
+
+  // // get cookie
+   function checkCookie () {
+      return "";
+   };
+
+  //save cookie
+   function saveAcceptInCookies () {
+    var imported = document.createElement('script');
+    imported.src = 'https://www.googletagmanager.com/gtag/js?id=G-CBJDNTG257';
+    document.head.appendChild(imported);
+
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-CBJDNTG257');
+   }
+
+});
